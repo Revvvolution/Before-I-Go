0df53b0b4f01b517a22cf34eeceb7d65d8e85496
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-06-21 11:14:16 -0400

    Added Register component functionality with new creator posting to database, as well as a function for password visibility toggling

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import { Authorized } from './views/Authorized.jsx'
 import { ApplicationViews } from './views/ApplicationViews.jsx'
 import { Login } from './components/auth/Login.jsx'
-/* import { Register } from './components/auth/Register.jsx' */
+import { Register } from './components/auth/Register.jsx'
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/register" element={<Register />} />  */}
+      <Route path="/register" element={<Register />} /> 
       
       <Route path="*" element={
         <Authorized>
---
+++ src/components/auth/Register.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+import { createUser, getCreatorByEmail } from "../../services/userService.jsx"
+
+export const Register = (props) => {
+
+  const uuid = crypto.randomUUID()
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [creator, setCreator] = useState({
+    id: "",
+    name: "",
+    email: "",
+    password: "",
+    viewcode: uuid
+  })
+  let navigate = useNavigate()
+
+  const registerNewCreator = () => {
+    createUser(creator).then((createdUser) => {
+      if (createdUser.hasOwnProperty("id")) {
+        localStorage.setItem(
+          "creator_user",
+          JSON.stringify({
+            id: createdUser.id,
+            name: createdUser.name,
+            viewcode: createdUser.viewcode
+          })
+        )
+
+        navigate("/")
+      }
+    })
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    getCreatorByEmail(creator.email).then((response) => {
+      if (response.length > 0) {
+        // Duplicate email. No good.
+        window.alert("Account with that email address already exists")
+      } else {
+        // Good email, create user.
+        registerNewCreator()
+      }
+    })
+  }
+
+  const updateCreator = (evt) => {
+    const copy = { ...creator }
+    copy[evt.target.id] = evt.target.value
+    setCreator(copy)
+  }
+  
+    const togglePasswordVisibility = () => {
+      setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+
+
+
+  return (
+    <main className="container-login" style={{ textAlign: "center" }}>
+      <form className="form-login" onSubmit={handleRegister}>
+        <h1>Before I Go</h1>
+        <h2>Please Register</h2>
+        <fieldset>
+          <div className="form-group">
+            <input
+              onChange={updateCreator}
+              type="text"
+              id="name"
+              className="form-control"
+              placeholder="Enter your name (First, Last)"
+              required
+              autoFocus
+            />
+          </div>
+        </fieldset>
+        <fieldset>
+          <div className="form-group">
+            <input
+              onChange={updateCreator}
+              type="email"
+              id="email"
+              className="form-control"
+              placeholder="Email address"
+              required
+            />
+          </div>
+        </fieldset>
+        <fieldset>
+          <div className="form-group-pass">
+            <input
+              onChange={updateCreator}
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              id="password"
+              className="form-control"
+              placeholder="Password"
+              required
+            />
+          </div>
+            <i 
+                className={`bi ${showPassword ? 'bi-eye' : 'bi-eye-slash'}`}
+                onClick={togglePasswordVisibility}
+                id="togglePassword"
+                ></i>
+        </fieldset>
+        <fieldset>
+          <div className="form-group">
+            <button className="login-btn btn-info" type="submit">
+              Register
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </main>
+  )
+}
